711821d250283de2148b48c1b7fea51f82cdf094
David <dav.baz@alice.it>
2021-04-11 11:47:54 +0200

    drop wasm

--- src/assets/scripts/example/pkg/example_bg.wasm.d.ts
+++
@@ -1,6 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export const memory: WebAssembly.Memory;
-export function get_input_buffer_pointer(): number;
-export function get_output_buffer_pointer(): number;
-export function amplify_audio(): void;
