25d623c003425abad94f0c63b2bca9dfc27046c4
David <dav.baz@alice.it>
2021-05-05 08:34:32 +0200

    clean

--- src/services/timer.service.ts
+++ src/services/timer.service.ts
@@ -64,17 +64,7 @@ export class TimerService {
     this.playingStateSource.next(true);
 
 
-    if (typeof Worker !== 'undefined') {
-      // Create a new
-      const worker = new Worker('./helper.worker', { type: 'module' });
-      worker.onmessage = ({ data }) => {
-        console.log(`page got message: ${data}`);
-      };
-      worker.postMessage('hello');
-    } else {
-      // Web Workers are not supported in this environment.
-      // You should add a fallback so that your program still executes correctly.
-    }
+
 
 
     this._ngZone.runOutsideAngular(() => {
